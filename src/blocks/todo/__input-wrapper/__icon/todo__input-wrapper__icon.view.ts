1140d2b6b6325448f5dd42b4fd1a08a128dcc3c8
vaniafed <ztankist@gmail.com>
2023-04-07 15:04:36 +0300

    refactoring

--- src/blocks/todo/__input-wrapper/__icon/todo__input-wrapper__icon.view.ts
+++ src/blocks/todo/__input-wrapper/__icon/todo__input-wrapper__icon.view.ts
@@ -1,9 +1,5 @@
 const clearInputIcon = document.querySelector('.todo__input-wrapper__icon')!;
 
-export const showClearInputIcon = () => {
-	clearInputIcon.classList.add('todo__input-wrapper_icon_shown');
-};
+export const showClearInputIcon = () => clearInputIcon.classList.add('todo__input-wrapper_icon_shown');
 
-export const hideClearInputIcon = () => {
-	clearInputIcon.classList.remove('todo__input-wrapper_icon_shown');
-};
+export const hideClearInputIcon = () => clearInputIcon.classList.remove('todo__input-wrapper_icon_shown');
